3d70a5f6f52ab3b2ff896718027fb0851030681d
CC007 <Coolcat_the_best2@Hotmail.com>
2024-04-21 23:52:17 +0200

    Scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/cf5c1a30-06bd-4ffa-ba83-5e22c8f22aaa-8gki00.png",
+  "https://utfs.io/f/917dde17-cf75-4734-a3bc-dd1153124aef-ogo2mb.png",
+  "https://utfs.io/f/68c77a46-8e7c-426a-8c40-619fe0284ad4-y3lkdn.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
-    </main>
+      <main className="">
+        <div className="flex flex-wrap gap-4">{
+          [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+              <div key={image.id} className="w-48">
+                <img src={image.url} />
+              </div>
+          ))
+        }</div>
+      </main>
   );
 }
